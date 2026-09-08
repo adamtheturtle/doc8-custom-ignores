81c5676a95a16b3b1b63a22f1d9a107df218fa85
Adam Dangoor <adamdangoor@gmail.com>
2026-09-08 12:06:29 +0100

    Use Ty's strictest recommended configuration

--- typings/doc8/checks.pyi
+++ typings/doc8/checks.pyi
@@ -1,13 +1,12 @@
-"""Partial types for doc8.checks."""
+
 
 from collections.abc import Iterator, Mapping
 from re import Pattern
 
 from doc8.parser import ParsedFile
 
-
 class ContentCheck:
-    """Base class for checks that inspect an entire file."""
+
 
     def __init__(self, cfg: Mapping[str, object]) -> None: ...
 
@@ -18,6 +17,6 @@ class ContentCheck:
 
 
 class CheckValidity(ContentCheck):
-    """doc8's reStructuredText validity check."""
+
 
     SPHINX_IGNORES_REGEX: list[Pattern[str]]
